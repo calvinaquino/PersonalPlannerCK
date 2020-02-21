f2914cd5bc55ae7604fef194687581b3bf825176
Calvin Goncalves de Aquino <calvin.de.aquino@clio.com>
2020-02-21 12:55:19 -0800

    Initial Commit

---
+++ personal-planner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  personal-planner
+//
+//  Created by Calvin De Aquino on 2020-02-21.
+//  Copyright © 2020 Calvin Aquino. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
