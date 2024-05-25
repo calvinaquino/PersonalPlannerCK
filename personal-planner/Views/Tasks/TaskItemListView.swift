c719801b7c0926b17d8ef47ff0c20bf3f5a7d5ba
Calvin Aquino <calving.aquino@gmail.com>
2024-05-24 19:33:48 -0700

    Create initial task view

---
+++ personal-planner/Views/Tasks/TaskItemListView.swift
@@ -0,0 +1,19 @@
+//
+//  TaskItemListView.swift
+//  personal-planner
+//
+//  Created by Calvin Gonçalves de Aquino on 2024-05-24.
+//  Copyright © 2024 Calvin Aquino. All rights reserved.
+//
+
+import SwiftUI
+
+struct TaskItemListView: View {
+    var body: some View {
+        Text("Tasks TBD")
+    }
+}
+
+#Preview {
+    TaskItemListView()
+}
